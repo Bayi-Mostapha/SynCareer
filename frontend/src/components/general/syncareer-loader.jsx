06a6f54e3917573917b28cd96aaefdb73759c9e9
skwizo <bayimostapha@gmail.com>
2024-03-31 14:50:31 +0100

    fixed profile

--- frontend/src/components/general/syncareer-loader.jsx
+++ frontend/src/components/general/syncareer-loader.jsx
@@ -1,6 +1,6 @@
-function SynCareerLoader() {
+function SynCareerLoader({ className }) {
     return (
-        <div className="flex gap-2">
+        <div className={`flex gap-2 ${className ? className : ''}`}>
             <div className="w-2 h-2 rounded-full bg-primary animate-jump"></div>
             <div className="w-2 h-2 rounded-full bg-primary animate-jump delay-100"></div>
             <div className="w-2 h-2 rounded-full bg-primary animate-jump delay-200"></div>
